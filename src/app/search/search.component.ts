fadb2483935f4f59549d7bb033c71ae8e787e0ce
abhinavshome <kabhinav.iitg@gmail.com>
2020-10-30 14:01:26 +0530

    3 files modified

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -44,4 +44,12 @@ export class SearchComponent implements OnInit {
       });
     });
   }
+
+  highlight(res) {
+    res.highlighted = true;
+  }
+
+  unhighlight(res) {
+    res.highlighted = false;
+  }
 }
